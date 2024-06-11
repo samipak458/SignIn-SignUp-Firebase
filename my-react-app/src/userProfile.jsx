1576931bec6e6cd1f9ba8d66393e0fffe181f27a
Muhammad Arsal Naveed <130850888+ArsalNaveed@users.noreply.github.com>
2024-06-12 00:36:50 +0500

    Update userProfile.jsx

--- my-react-app/src/userProfile.jsx
+++ my-react-app/src/userProfile.jsx
@@ -1,61 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { db } from './firebase';
-import { collection, doc, getDocs, onSnapshot } from "firebase/firestore"; 
+import { doc, onSnapshot } from "firebase/firestore"; 
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
+const UserProfile = () => {
+  const [userData, setUserData] = useState(null);
 
-const UserProfile = ({ username, email, advance }) => {
-    const [userData, setUserData] = useState(null);
-    
-    
-    useEffect(() => {
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log('User is signed in : ' + user.uid);
+        const docRef = doc(db, 'users', user.uid);
 
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                console.log('User is signed in : ' + user.uid);
-                const docRef = doc(db, 'users', user.uid);
-
-                console.log(docRef);
-
-                onSnapshot(docRef, (def) => {
-                    console.log('Current data: ', def.data());
-                    const userData = def.data();
-                    setUserData(userData);
-                });
-            } else {
-                console.log('No user is signed in');
-            }
+        onSnapshot(docRef, (doc) => {
+          console.log('Current data: ', doc.data());
+          setUserData(doc.data());
         });
-        }, []);
+      } else {
+        console.log('No user is signed in');
+      }
+    });
+  }, []);
 
-    const handleSignOut = () => { 
-        const auth = getAuth();
-        signOut(auth).then(() => {
-            console.log('User signed out');
-            window.location.href = '/login';
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
-   
+  const handleSignOut = () => { 
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log('User signed out');
+      window.location.href = '/login';
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
 
-    return (
+  return (
+    <div>
+      <h2>User Profile</h2>
+      {userData && (
         <div>
-            <h2>User Profile</h2>
-            {userData && (
-                <div>
-                    <p>Username: {userData.username}</p>
-                    <p>Email: {userData.email}</p>
-                    <p>Subscribed: {userData.paid}</p>
-                </div>
-            )}
-
-            <button onClick={handleSignOut}>Sign Out</button>
+          <p>Username: {userData.username}</p>
+          <p>Email: {userData.email}</p>
+          <p>Subscribed: {userData.paid}</p>
         </div>
-    );
+      )}
+      <button onClick={handleSignOut}>Sign Out</button>
+    </div>
+  );
 };
-
-
 
 export default UserProfile;

4b0118c9da7e46aa580cb30bbd508feea3f7e51f
Muhammad Arsal Naveed <130850888+ArsalNaveed@users.noreply.github.com>
2024-06-12 00:28:24 +0500

    Update firebase.js

--- my-react-app/src/firebase.js
+++ my-react-app/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; //google auth
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,6 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app); //google
 const db = getFirestore(app);
-export {auth, db};
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, db, googleProvider }; //google

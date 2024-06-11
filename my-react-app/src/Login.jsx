a3e1fc1c1a482c8aa4d114f1b6674d50a64de6ca
Muhammad Arsal Naveed <130850888+ArsalNaveed@users.noreply.github.com>
2024-06-12 00:29:45 +0500

    Update Login.jsx

--- my-react-app/src/Login.jsx
+++ my-react-app/src/Login.jsx
@@ -1,48 +1,50 @@
 import React, { useState } from 'react';
-import {auth} from './firebase';
+import { auth } from './firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
-
+import GoogleSignInButton from './GoogleSignInButton';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await signInWithEmailAndPassword(auth, email, password)
-            console.log('User logged in');
-            alert('User logged in')
-            window.location.href = '/userProfile';
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log('User logged in');
+      alert('User logged in');
+      window.location.href = '/userProfile';
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Email:</label>
-                <input type="email" value={email} onChange={handleEmailChange} />
-                <br />
-                <label>Password:</label>
-                <input type="password" value={password} onChange={handlePasswordChange} />
-                <br />
-                <button type="submit">Login</button>
-                <br />
-                <p>Not registered? <a href="/signup">SignUp</a></p>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={handleEmailChange} />
+        <br />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+        <br />
+        <button type="submit">Login</button>
+        <br />
+        <GoogleSignInButton />
+        <br />
+        <p>Not registered? <a href="/signup">SignUp</a></p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
